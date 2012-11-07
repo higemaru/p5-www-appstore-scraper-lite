54309a4b1a3185e3b23e6d2f056e9266b7c17de6
KAWABATA Kazumichi <kawabata@cpan.org>
2012-11-07 22:14:35 +0900

    add Makefile.PL, Changes, README

--- lib/WWW/AppStore/Scraper/Lite.pm
+++ lib/WWW/AppStore/Scraper/Lite.pm
@@ -8,6 +8,8 @@ use LWP::UserAgent;
 use XML::Simple;
 use JSON;
 
+our $VERSION = '0.11';
+
 sub new {
     my $class = shift;
     my @args = @_;
@@ -505,3 +507,153 @@ sub _init_countries {
 }
 
 1;
+__END__
+
+=head1 NAME
+
+WWW::AppStore::Scraper::Lite - Get software review/rate on AppStore.
+
+=head1 SYNOPSIS
+
+  use WWW::AppStore::Scraper::Lite;
+  use Data::Dumper;
+
+  my $obj = WWW::AppStore::Scraper::Lite->new(wait => 5);
+
+  my $info = $obj->app_info(
+  			    app => ['404732112'],
+			    store => ['jp','us'],
+			    review_number => 1,
+                           );
+
+  print Dumper $info;
+
+  # result
+  # $VAR1 = {
+  #           '404732112' => {
+  #                            'jp' => {
+  #                                      'review_number' => 1,
+  #                                      'store' => 'jp',
+  #                                      'reviews' => [
+  #                                                     {
+  #                                                       'date' => '2012-11-05',
+  #                                                       'title' => "......",
+  #                                                       'message' => "......"
+  #                                                     }
+  #                                                   ],
+  #                                      'store_code' => 143462,
+  #                                      'genre_id' => 6002,
+  #                                      'app_name' => "Sleipnir Mobile - Web \x{30d6}\x{30e9}\x{30a6}\x{30b6}",
+  #                                      'store_name' => 'Japan',
+  #                                      'artist_id' => 318578225,
+  #                                      'ratings' => {
+  #                                                     'userRatingCount' => 1727,
+  #                                                     'averageUserRatingForCurrentVersion' => '4.5',
+  #                                                     'averageUserRating' => '3.5',
+  #                                                     'userRatingCountForCurrentVersion' => 4
+  #                                                   },
+  #                                      'price' => '0',
+  #                                      'genre_name' => 'Utilities'
+  #                                    },
+  #                            'us' => {
+  #                                      'review_number' => 1,
+  #                                      'store' => 'us',
+  #                                      'reviews' => [
+  #                                                     {
+  #                                                       'date' => '2012-09-08',
+  #                                                       'title' => '......',
+  #                                                       'message' => '.......'
+  #                                                     }
+  #                                                   ],
+  #                                      'store_code' => 143441,
+  #                                      'genre_id' => 6002,
+  #                                      'app_name' => 'Sleipnir Mobile - Web Browser',
+  #                                      'store_name' => 'United States',
+  #                                      'artist_id' => 318578225,
+  #                                      'ratings' => {
+  #                                                     'userRatingCount' => 220,
+  #                                                     'averageUserRatingForCurrentVersion' => undef,
+  #                                                     'averageUserRating' => '4.5',
+  #                                                     'userRatingCountForCurrentVersion' => undef
+  #                                                   },
+  #                                      'price' => '0',
+  #                                      'genre_name' => 'Utilities'
+  #                                    }
+  #                          }
+  #         };
+
+=head1 DESCRIPTION
+
+App information is provided Apple Search API (JSON).
+App reviews is provided RSS.
+WWW::AppStore::Scraper::Lite get both.
+
+=head1 Methods
+
+=head2 new
+
+=over 4
+
+blah blah
+
+=over 4
+
+=item wait
+
+set interval (second). per app, per store.
+
+=back
+
+=back
+
+=head2 app_info
+
+=over 4
+
+Get application information.
+
+=over 4
+
+=item app
+
+set application identifier code.
+
+ex.
+  app => '404732112',
+  app => ['404732112', '531254369'],
+ ......
+
+=item store
+
+set store_code. By default, get info from all country's stores.
+
+NOTE: I suggest you should set one or two store_code.
+
+ex.
+  store => 'jp',
+  store => ['jp','us'],
+  ......
+
+=back
+
+=back
+
+=head2 app_base_info
+
+Get application information without review.
+
+=head1 AUTHORS
+
+KAWABATA, Kazumichi (Higemaru) E<lt>kawabata@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+=head1 SEE ALSO
+
+Search API: L<http://www.apple.com/itunes/affiliates/resources/documentation/itunes-store-web-service-search-api.html>
+
+=cut
